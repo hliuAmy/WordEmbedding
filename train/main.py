f7f732b923864c9fb6456178baabbf6e58590e09
hliuAmy <32031588+hliuAmy@users.noreply.github.com>
2017-12-13 14:33:19 +0800

    alpha+(1-alpha) last update

--- train/main.py
+++ train/main.py
@@ -23,19 +23,19 @@ def readFile(path):
     return G
 
 def combinationVec(sourceVec,distinationVec,alpha):
+    result={}
     for key in sourceVec:
-        sourceVec[key]=alpha*sourceVec[key]+(1-alpha)*distinationVec[key]
-    return sourceVec
+        result[key]=alpha*sourceVec[key]+(1-alpha)*distinationVec[key]
+    return result
 
 def main(total_iter, dim, topicNum, dataset,alpha):
-    '''
-    total_iter:number of sample edge
-    '''
-    input_wordsG_path = '../data_preparation/result_graph/' + \
-        dataset + '/wordsG_tf_count.data'
+    # '''
+    # total_iter:number of sample edge
+    # '''
+    input_wordsG_path = '../data_preparation/result_graph/' +  dataset + '/wordsG_tf_count.data'
     input_topicG_path = '../data_preparation/result_graph/' + dataset + '/topicG.data'
-    emb_words_path = '../result/' + dataset + '/onlywords.emb_noM_'
-    emb_topic_words_path = '../result/' + dataset + '/words.emb_noM_'
+    emb_words_path = '../result/' + dataset + '/onlywords.emb_noM'
+    emb_topic_words_path = '../result/' + dataset + '/words.emb_noM'
     emb_cmb_words_path = '../result/' + dataset + '/cmb_words.emb_noM_'
 
     print("*****Read Data*****")
@@ -49,13 +49,15 @@ def main(total_iter, dim, topicNum, dataset,alpha):
     trainG.initial()
     trainGT = embTrain(topicG, dim, 't')
     trainGT.initial()
+    trainG_temp = embTrain(wordsG, dim, 'w')
+    trainG_temp.initial()
 
     print("*****train*****")
-    wordsVec_init=trainG.wordsVec
     trainG.trainW(total_iter)
     trainG.output(emb_words_path, trainG.wordsVec)
-    wordsVecT=trainGT.trainT_noM(total_iter, wordsVec_init, trainG.G)
+    wordsVecT=trainGT.trainT_noM(total_iter, trainG_temp.wordsVec, trainG.G)
     trainGT.output(emb_topic_words_path, wordsVecT)
+
 
     print('******combination*****')
     for alpha in [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]:
@@ -63,7 +65,19 @@ def main(total_iter, dim, topicNum, dataset,alpha):
         trainG.output(emb_cmb_words_path+str(alpha),cmbVec)
 
 
+    #try tf or tfidf for wordsG;we test five times for embedding
+    # input_wordsG_path = '../data_preparation/result_graph/' +  dataset + '/wordsG_tfidf.data'
+    # emb_words_path = '../result/' + dataset + '/emb_tfidf'
+    # wordsG = readFile(input_wordsG_path)
+    # print(dataset + "'s wordsG's number of edges is ", len(wordsG.edges()))
+    # for i in range(5):
+    #     trainG = embTrain(wordsG, dim, 'w')
+    #     trainG.initial()
+    #     trainG.trainW(total_iter)
+    #     trainG.output(emb_words_path+str(i), trainG.wordsVec)
+
+
 if __name__ == '__main__':
     datasets = ['KDD']
     for dataset in datasets:
-        main(total_iter=10000000, dim=200, topicNum=50, dataset=dataset,alpha=0.5)
+        main(total_iter=1000000, dim=200, topicNum=50, dataset=dataset,alpha=0.5)
