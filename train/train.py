8f6a4d0759ce0ce82bfd390ce386c5670cff9ed6
hliuAmy <32031588+hliuAmy@users.noreply.github.com>
2017-12-19 15:19:06 +0800

    no initial value and alone for train

--- train/train.py
+++ train/train.py
@@ -1,7 +1,7 @@
 
-# main
 import networkx as nx
 from updateW import *
+from updateT import *
 from updateWT import *
 
 
@@ -33,32 +33,54 @@ def combinationVec(sourceVec, distinationVec, alpha):
     return result
 
 
-def main(total_iter, dim, dataset,alpha,beta):
+def main(total_iter, dim, dataset, alpha, beta):
     # '''
     # total_iter:number of sample edge
     # '''
-    input_wordsG_path = '../data_preparation/result_graph/' + \
+    input_wordsG_path = '../data_preparation/result_graph/' + dataset + '/wordsG_tf.data'
+    input_wordsG_path1 = '../data_preparation/result_graph/' + \
         dataset + '/wordsG_tf_count.data'
-    input_topicG_path = '../data_preparation/result_graph/' + dataset + '/topicG.data'
-    result_path = '../result/' + dataset + '/emb_beta_'+str(beta)
+    input_topicG_path = '../data_preparation/result_graph/' + dataset + '/topicG100.data'
+    input_topicG_path1 = '../data_preparation/result_graph/' + dataset + '/topicG.data'
+    trainG_path = '../result/' + dataset + '/trainG_tf.emb'
+    trainG_path1 = '../result/' + dataset + '/trainG_tf_count.emb'
+    trainT_path = '../result/' + dataset + '/trainT100_noM.emb'
+    trainT_path1 = '../result/' + dataset + '/trainT50_noM.emb'
+    result_path = '../result/' + dataset + '/emb23'
 
     print("*****Read Data*****")
     wG = readFile(input_wordsG_path)
     print(dataset + "'s wordsG's number of edges is ", len(wG.edges()))
+    wG1 = readFile(input_wordsG_path1)
+    print(dataset + "'s wordsG's number of edges is ", len(wG1.edges()))
     wtG = readFile(input_topicG_path)
     print(dataset + "'s topicG's number of edges is ", len(wtG.edges()))
+    wtG1 = readFile(input_topicG_path1)
+    print(dataset + "'s topicG's number of edges is ", len(wtG1.edges()))
 
     print("*****train*****")
-    trainG = updateW(wG, dim)
-    trainG.initial()
-    trainG.trainW(total_iter)
-    trainGT = embTrain(wG, wtG, trainG.wordsVec, dim)
-    trainGT.initial()
-    trainGT.train(total_iter, result_path,alpha,beta)
+    # trainG = updateW(wG, dim)
+    # trainG.initial()
+    # trainG.trainW(total_iter)
+    # trainG.output(trainG_path, trainG.wordsVec)
+    # trainG1 = updateW(wG1, dim)
+    # trainG1.initial()
+    # trainG1.trainW(total_iter)
+    # trainG1.output(trainG_path1, trainG1.wordsVec)
+
+    trainT = updateT(wtG, None, dim)
+    trainT.initial()
+    trainT.train(total_iter, trainT_path)
+    trainT1 = updateT(wtG1, None, dim)
+    trainT1.initial()
+    trainT1.train(total_iter, trainT_path1)
+
+    # trainGT = embTrain(wG, wtG, trainT.wordsVec, trainT.topicsVec, dim)
+    # trainGT.initial()
+    # trainGT.train(total_iter, result_path, alpha, beta)
 
 
 if __name__ == '__main__':
-    datasets = ['KDD']
+    datasets = ['KDD', 'WWW']
     for dataset in datasets:
-        for i in [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]:
-            main(total_iter=1000000, dim=200, dataset=dataset,alpha=0,beta=i)
+        main(total_iter=1000000, dim=200, dataset=dataset, alpha=0, beta=0)
--- train/updateT.py
+++ train/updateT.py
@@ -6,29 +6,23 @@ import networkx as nx
 import csv
 
 
-class embTrain:
+class updateT:
 
-    def __init__(self, wG, wtG, wordsVec, topicsVec, dim):
-        self.wG = wG
+    def __init__(self, wtG, wordsVec, dim):
         self.wtG = wtG
         self.dim = dim
-        self.wordsVec = wordsVec
-        self.contextVec = {}
-        self.topicsVec = topicsVec
-        #self.topicsVec = {}
-        self.edges_weight = []
-        self.edges = []
-        self.nodes_weight = {}
+        if wordsVec == None:
+            self.wordsVec = {}
+        else:
+            self.wordsVec = wordsVec
+        self.topicsVec = {}
         self.edges_weight_t = []
         self.edges_t = []
         self.nodes_weight_t = {}
         self.sigmoid_table = []
-        self.alias = []
-        self.prob = []
         self.alias_t = []
         self.prob_t = []
         self.neg_table_t = []
-        self.neg_table = []
         self.SIGMOID_BOUND = 6
         self.sigmoid_table_size = 1000
         self.neg_table_size = 1000000
@@ -39,17 +33,6 @@ class embTrain:
         #self.M = np.eye(self.dim)
 
     def readG(self):
-        for u, v, d in self.wG.edges(data=True):
-            w = float(d['weight'])
-            self.edges_weight.append(w)
-            self.edges.append((u, v, w))
-            if u in self.nodes_weight.keys():
-                self.nodes_weight[u] += w
-            else:
-                self.nodes_weight[u] = w
-        for node in self.wG.nodes():
-            self.contextVec[node] = np.zeros(self.dim)
-
         for u, v, d in self.wtG.edges(data=True):
             w = float(d['weight']) * 10
             self.edges_weight_t.append(w)
@@ -58,65 +41,14 @@ class embTrain:
                 self.nodes_weight_t[v] += w
             else:
                 self.nodes_weight_t[v] = w
-        # for node in self.wtG.nodes():
-        #     if node.startswith('T'):
-        #         self.topicsVec[node] = np.random.uniform(
-        #             low=-0.5 / self.dim, high=0.5 / self.dim, size=(self.dim))
-
-    def initAliasTable(self):
-        length = len(self.edges_weight)
-        norm_prob = []
-        large_block = []
-        small_block = []
-        self.prob = [0.0] * length
-        self.alias = [0.0] * length
-        num_small_block = 0
-        num_large_block = 0
-        sum_weight = sum(self.edges_weight)
-        for i in range(length):
-            norm_prob.append(self.edges_weight[i] * length / sum_weight)
-        for i in range(length)[::-1]:
-            if norm_prob[i] < 1:
-                small_block.append(i)
-                num_small_block += 1
-            else:
-                large_block.append(i)
-                num_large_block += 1
-        while num_large_block > 0 and num_small_block > 0:
-            num_small_block -= 1
-            num_large_block -= 1
-            cur_small_block = small_block[num_small_block]
-            cur_large_block = large_block[num_large_block]
-            self.prob[cur_small_block] = norm_prob[cur_small_block]
-            self.alias[cur_small_block] = cur_large_block
-
-            large_prob = norm_prob[cur_large_block]
-            small_prob = norm_prob[cur_small_block]
-            norm_prob[cur_large_block] = large_prob + small_prob - 1
-            if(norm_prob[cur_large_block] < 1):
-                small_block[num_small_block] = cur_large_block
-                num_small_block += 1
-            else:
-                large_block[num_large_block] = cur_large_block
-                num_large_block += 1
-        while(num_large_block > 0):
-            num_large_block -= 1
-            index = large_block[num_large_block]
-            self.prob[index] = 1.0
-        while(num_small_block > 0):
-            num_small_block -= 1
-            index = small_block[num_small_block]
-            self.prob[index] = 1.0
-        del norm_prob
-        del small_block
-        del large_block
-
-    def sampleAnEdge(self, rand_value1, rand_value2):
-        k = len(self.edges_weight) * rand_value1
-        if rand_value2 < self.prob[int(k)]:
-            return k
-        else:
-            return self.alias[int(k)]
+        if len(self.wordsVec.keys()) == 0:
+            for node in self.wtG.nodes():
+                self.wordsVec[node] = np.random.uniform(
+                    low=-0.5 / self.dim, high=0.5 / self.dim, size=(self.dim))
+        for node in self.wtG.nodes():
+            if node.startswith('T'):
+                self.topicsVec[node] = np.random.uniform(
+                    low=-0.5 / self.dim, high=0.5 / self.dim, size=(self.dim))
 
     def initAliasTable_t(self):
         length = len(self.edges_weight_t)
@@ -173,24 +105,6 @@ class embTrain:
         else:
             return self.alias_t[int(k)]
 
-    def initNegTable(self):
-        ssum = 0.0
-        cur_sum = 0.0
-        por = 0.0
-        i = 0
-        neg_sampling_power = 0.75
-        for value in self.nodes_weight.values():
-            try:
-                ssum += math.pow(value, neg_sampling_power)
-            except ValueError as err:
-                print(value)
-        for word in self.nodes_weight.keys():
-            cur_sum += math.pow(self.nodes_weight[word], neg_sampling_power)
-            por = cur_sum / ssum
-            while i < self.neg_table_size and float(i) / self.neg_table_size < por:
-                self.neg_table.append(word)
-                i += 1
-
     def initNegTable_t(self):
         ssum = 0.0
         cur_sum = 0.0
@@ -227,14 +141,12 @@ class embTrain:
 
     def initial(self):
         self.readG()
-        self.initAliasTable()
-        self.initNegTable()
         self.initAliasTable_t()
         self.initNegTable_t()
         self.initSigmoidTable()
         print('init is ok')
 
-    def train(self, total_iter, outputPath, alpha, beta):
+    def train(self, total_iter, outputPath):
         for i in range(total_iter):
             if i % 100000 == 0:
                 print(i)
@@ -243,53 +155,14 @@ class embTrain:
                 if self.rho < self.init_rho * 0.0001:
                     self.rho = self.init_rho * 0.0001
                 print(i, ':', self.rho)
-            self.trainW(alpha)
-            self.trainT(alpha, beta)
             # self.trainT_noM()
+            self.trainT_noM()
             if i == total_iter - 1:
                 self.output(outputPath, self.wordsVec)
 
-    def trainW(self, alpha):
-        vec_error = np.zeros(self.dim)
-        random1 = np.random.random()
-        random2 = np.random.random()
-        edgeID = int(self.sampleAnEdge(random1, random2))
-        edge = self.edges[edgeID]
-        u = edge[0]
-        v = edge[1]
-        w = float(edge[2])
-        label = 0
-        target = ''
-        for i in range(self.num_negative + 1):
-            if i == 0:
-                label = 1
-                target = v
-            else:
-                neg_index = int(self.neg_table_size * np.random.random())
-                target = self.neg_table[neg_index]
-                if u == None or v == None or target == None:
-                    print(u, v, neg_index, target)
-                if target == u or target == v:
-                    i -= 1
-                    continue
-                label = 0
-            x = np.dot(self.wordsVec[u], self.contextVec[target])
-            g = (label - self.FastSigmoid(x)) * self.rho
-            vec_error += g * self.contextVec[target]
-            self.contextVec[target] += g * self.wordsVec[u]
-        self.wordsVec[u] = self.wordsVec[u] + \
-            vec_error + 2 * alpha * self.wordsVec[u]
-
-    def trainT(self, alpha, beta):
+    def trainT(self):
         vec_error = np.zeros(self.dim)
         M_error = np.zeros((self.dim, self.dim))
-        # signM = np.zeros((self.dim, self.dim))
-        # for m in range(self.dim):
-        #     for n in range(self.dim):
-        #         if self.M[m][n] >0:
-        #             signM[m][n] = 1
-        #         if self.M[m][n] <0:
-        #             signM[m][n] = -1
 
         def choiceEdge():
             random1 = np.random.random()
@@ -320,14 +193,12 @@ class embTrain:
                 label = 0
             x = np.dot(
                 np.dot(self.wordsVec[u].T, self.M), self.topicsVec[target])
-            # print(x)
             g = (label - self.FastSigmoid(x)) * self.rho
             vec_error = vec_error + g * np.dot(self.M, self.topicsVec[target])
             M_error = M_error + g * \
                 np.dot(self.wordsVec[u], self.topicsVec[target].T)
             self.topicsVec[target] += g * np.dot(self.M, self.wordsVec[u])
         self.wordsVec[u] = self.wordsVec[u] + vec_error
-        #self.M = self.M + M_error + beta * signM
         self.M = self.M + M_error
 
     def trainT_noM(self):
